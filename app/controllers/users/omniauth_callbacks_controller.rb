81188edf5aa4aa3e3990bb017a3996499e6106f5
walimike <walimike139@gmail.com>
2025-02-13 13:16:24 +0300

    auth pages

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,23 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def google_oauth2
+    user = User.from_omniauth(request.env['omniauth.auth'])
+
+    if user.present?
+      sign_out_all_scopes
+      sign_in_and_redirect user, event: :authentication
+      set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
+    else
+      session['devise.google_data'] = request.env['omniauth.auth'].except(:extra)
+      redirect_to new_user_session_path, alert: 'There was a problem signing you in through Google. Please register or try signing in later.'
+    end
+  end
+
+  def failure
+    redirect_to root_path, alert: 'Authentication failed, please try again.'
+  end
+
+  private
+  def auth
+    @auth ||= request.env['omniauth.auth']
+  end
+end
